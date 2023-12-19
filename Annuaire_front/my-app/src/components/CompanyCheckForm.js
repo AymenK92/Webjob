66f0dccd3da53dc41c95e570f7373a828c3c95f3
AymenK92 <146014631+AymenK92@users.noreply.github.com>
2023-12-19 23:12:58 +0100

    Update CompanyCheckForm.js

--- Annuaire_front/my-app/src/components/CompanyCheckForm.js
+++ Annuaire_front/my-app/src/components/CompanyCheckForm.js
@@ -21,6 +21,11 @@ const getCsrfTokenFromCookies = () => {
   return null;
 };
 
+const csrfToken = getCsrfTokenFromCookies();
+    console.log("CSRF Token in App:", csrfToken);
+  }, []);
+
+
 const CompanyCheckForm = ({ onCompanyExists, onCompanyNotFound }) => {
   const { updateCompanyData } = useContext(CompanyFormContext);
   const [formData, setFormData] = useState({ name: '', location: '' });
