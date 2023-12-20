524eb746cde62929ec5a9b9fac2ef705e6538d71
AymenK92 <146014631+AymenK92@users.noreply.github.com>
2023-12-20 01:34:13 +0100

    Update CompanyFormModal.js

--- Annuaire_front/my-app/src/components/CompanyFormModal.js
+++ Annuaire_front/my-app/src/components/CompanyFormModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'; 
+import React, { useState, useContext, useEffect } from 'react'; 
 import { Modal, Box, TextField, Button } from '@mui/material';
 import axios from 'axios';
 import { CompanyFormContext } from '../contexts/CompanyFormContext';
